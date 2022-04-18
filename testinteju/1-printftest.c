17366fb21abb281616a0a9c22292ac18a413d49f
Tech-sis <abilawonadeola@gmail.com>
2022-04-18 08:29:01 -0700

    final test

--- testinteju/1-printftest.c
+++ testinteju/1-printftest.c
@@ -10,47 +10,43 @@
 int _printf(const char *format, ...)
 {
 	int i, j, d, total = 0;
-    va_list args;
-    char c, f, *s;
+	va_list args;
+	char c, *s;
 
-    va_start(args, format);
-    for (i = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            if (format[i] == '%')
-            {
-                write(1, "%", 1);
-                total++;
-            }
-            else if (format[i] == 'd' || f == 'i')
-            {
-                d = va_arg(args, int);
-                total += print_int(d);
-            }
-            else if (format[i] == 's')
-            {
-                s = va_arg(args, char *);
-                for (j = 0; s[j] != '\0'; j++)
-                {
-                    write(1, &s[j], 1);
-                    total++;
-                }
-            }
-            else if (format[i] == 'c')
-            {
-                c = va_arg(args, int);
-                write(1, &c, 1);
-                total++;
-            }
-        }
-        else
-        {
-            write(1, &format[i], 1);
-            total++;
-        }
-    }
-    va_end(args);
-    return (total);
+	va_start(args, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '%')
+			{
+				write(1, "%", 1);
+				total++;
+			} else if (format[i] == 'd' || format[i] == 'i')
+			{
+				d = va_arg(args, int);
+				total += print_int(d);
+			} else if (format[i] == 's')
+			{
+				s = va_arg(args, char *);
+				for (j = 0; s[j] != '\0'; j++)
+				{
+					write(1, &s[j], 1);
+					total++;
+				}
+			} else if (format[i] == 'c')
+			{
+				c = va_arg(args, int);
+				write(1, &c, 1);
+				total++;
+			}
+		} else
+		{
+			write(1, &format[i], 1);
+			total++;
+		}
+	}
+	va_end(args);
+	return (total);
 }
--- testinteju/print_num.c
+++ testinteju/print_num.c
@@ -7,32 +7,32 @@
 
 int print_int(int d)
 {
-    int i, len = 1, num, total = 0;
-    char c;
+	int i, len = 1, num, total = 0;
+	char c;
 
-    if (d < 0)
-    {
-        write(1, "-", 1);
-        total++;
-        d *= -1;
-    }
+	if (d < 0)
+	{
+		write(1, "-", 1);
+		total++;
+		d *= -1;
+	}
 
-    num = d;
+	num = d;
 
-    for (i = 0; num > 9; i++)
-    {
-        len *= 10;
-        num /= 10;
-    }
+	for (i = 0; num > 9; i++)
+	{
+		len *= 10;
+		num /= 10;
+	}
 
-    while (i >= 0)
-    {
-        c = d / len + '0';
-        write(1, &c, 1);
-        total++;
-        d %= len;
-        len /= 10;
-        i--;
-    }
-    return total;
+	while (i >= 0)
+	{
+		c = d / len + '0';
+		write(1, &c, 1);
+		total++;
+		d %= len;
+		len /= 10;
+		i--;
+	}
+	return (total);
 }
